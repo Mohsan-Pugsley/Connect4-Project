377d67c432faf8fea15c21b30298adc30e49c3a5
parisgibbs3 <a1801311@adelaide.edu.au>
2022-05-12 11:02:20 +0930

    testing

--- Menu.cpp
+++ Menu.cpp
@@ -4,29 +4,31 @@ using namespace std;
 
 Menu::Menu(){
     playerOption = 0;
-
+    boardRows = 0;
+    boardColumns = 0;
 }
-
 
 Menu::displayMenu(){
     // display menu
+    int playerOption=0;
     cout << "\t\tWelcome to Connect 4!\n\n";
     cout << "Please Select a Player Option" << endl;
     cout << "1. Person vs. Person" << endl;
     cout << "2. Person vs. Computer" << endl;
+}
+
+Menu::setPlayerOption(int newPlayerOption){
     cout << "Enter the number for the corresponding option above: " << endl;
     cin >> playerOption;
 
-    // respong to user's choice
-    if(playerOption == 1){
-        cout << "You have selected a Person opponent" << endl;
-        // .playGame()
-    }
-    else if(playerOption == 2){
-        cout << "You have selected a Computer opponent" << endl;
-        // .playGame()
-    }
-    else{
-        cout << "Please enter a valid option" << endl;
-    }
+    playerOption = newPlayerOption;
 }
+
+Menu::getPlayerOption(){
+    return newPlayerOption;
+}
+
+
+
+
+
--- Menu.h
+++ Menu.h
@@ -8,12 +8,15 @@ class Menu{
     int boardRows;
     int boardColumns;
     public:
-        Menu(); \\ Constructs
-        void displayMenu(); \\ Displays the main menu
-        void setboardRows(int newBoardRows); \\ Sets the number of rows from user 
-        int getboardRows(); \\ Returns the number of rows
-        void setboardColumns(int newBoardColumns); \\ Sets the number of columns from user
-        int getboardColumns(); \\ Returns the number of columns
+        Menu(); // Constructs
+        void displayMenu(); // Displays the main menu
+        void setPlayerOption(int newPlayerOption); // Sets the player option
+        int getPlayerOption(); // Returns the player option
+        void setBoardSize(int newBoardRows, int newBoardColumns);
+        int getBoardRows(); // Returns the number of rows
+        void setBoardColumns(int newBoardColumns); // Sets the number of columns from user
+        int getBoardColumns(); // Returns the number of columns
+        ~Menu();
 };
 
 #endif
