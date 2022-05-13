022c125014f4392258ae5e56d8850da1f9ed801b
parisgibbs3 <a1801311@adelaide.edu.au>
2022-05-13 14:01:15 +0930

    changes

--- Game.h
+++ Game.h
@@ -3,21 +3,18 @@
 #include <iostream>
 #include "Menu.h"
 #include "Board.h"
-
+#include "Computer.h"
+#include "Person.h"
+ 
 class Game {
     private:
         int gameState;
         bool gameRunning;
-        //Player player1;
-        //Player player2;
-
-        bool player1Won;
-        bool player2Won;
-        bool gameDraw;
-
+        Person player1;
+        Computer computer;
         Menu menu;
         Board board;
-
+ 
     public:
         Game();
         void initialize();
@@ -26,16 +23,7 @@ class Game {
         int getState();
         void draw();
         void refresh();
-
-        void p1WonCondition();
-        void p2WonCondition();
-        void drawCondition();
-
-        bool getP1WonFlag();
-        bool getP2WonFlag();
-        bool getDrawFlag();
-
         ~Game();
 };
-
+ 
 #endif
