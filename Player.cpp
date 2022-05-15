a401ef7fd91bf636455d442f5d83846787007fec
parisgibbs3 <a1801311@adelaide.edu.au>
2022-05-15 11:34:24 +0930

    no changes made

--- Player.cpp
+++ Player.cpp
@@ -1,73 +1,29 @@
-#include "Player.h"
-#include "Person.h"
-#include "Computer.h"
-#include "Menu.h"
-
+#ifndef PLAYER_H
+#define PLAYER_H
 #include <iostream>
 using namespace std;
+class Player{
+    int player1;
+    int player2;
+    int computer;
+    int comRandCol;
+    int comColCount;
+    int comLastMoveCol;
+    
 
-Player::Player(){
-    player1=0;
-    player2=0;
+    public:
+        Player(); //constructor
+        void p1Input();
+        void p2Input();
+        void cInput();
+        int getP1Move();
+        int getP2Move();
+        int getCMove();
 
-    computer=0;
-    comRandCol=0;
-    comColCount=0;
-    comLastMoveCol=0;
-}
+        //virtual void move() = 0;
+        //virtual void move() = 0; //commented virtual to compile menu,board,player and main-board cpp
 
-void Player::p1Input(){
-    cout<<"player 1 move: "; 
-    cin>>player1;
-}
+        bool win(Player*opponent);
 
-void Player::p2Input(){
-    cout<<"player 2 move: "; 
-    cin>>player2;
-}
-
-void Player::cInput(){
-
-    cout<<"computer move";
-
-    comRandCol = rand() % comColCount + 1; // + 1 offsets from 0
-     if (comRandCol < comLastMoveCol) {
-         if (rand() % 3 + 1 < 3) { // 2/3rds of the time
-             comRandCol++; // move the randomCol closer to lastMoveCol
-         }
-     } else if (comRandCol > comLastMoveCol) {
-         if (rand() % 3 + 1 < 3) {
-             comRandCol--; // move the randomCol closer to lastMoveCol
-         }
-     } // current result of this is that the computer chooses the edge columns less as well
-
-    cout<<comRandCol;
-
-    computer = comRandCol;
-}
-        
-int Player::getP1Move(){
-    return player1;
-}
-        
-int Player::getP2Move(){
-    return player2;
-}
-
-int Player::getCMove(){
-    return computer;
-}
-
-
-//void Player::setPlayer1(){ - Paris: Just thinking how to set each player as a person or computer depending on the selected playerOption
-//    Player1 * = new Person();
-//}
-
-//void Player::setPlayer2(){
-//    if(playerOption == 1){
-//        Player 2 * = new Person();
-//    }
-//    else if(playerOption == 2){
-//        Player 2 * = new Computer();
-//    }
-//}
+};
+#endif
