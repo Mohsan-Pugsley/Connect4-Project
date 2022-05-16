42d09fbfa7debea26f5d54b4370ef4ed2f0a69cc
viviana <a1810844@adelaide.edu.au>
2022-05-17 01:44:35 +0930

    test

--- Game.h
+++ Game.h
@@ -1,6 +1,11 @@
 #ifndef GAME_H
 #define GAME_H
 #include <iostream>
+#include "Menu.h"
+#include "Board.h"
+#include "Player.h"
+#include "Computer.h"
+#include "Person.h"
 #include <variant>
 
 class Game {
@@ -13,8 +18,8 @@ class Game {
         bool player1Won;
         bool player2Won;
         bool gameDraw;
-        Player* player1;
-        Player* player2;
+        Player *player1;
+        Player *player2;
         Menu menu;
         Board B;
 
